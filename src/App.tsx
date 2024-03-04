916e2d180a11ff6d4f29def187aae1cd75b08a85
modasser-nayem <modassernayem@gmail.com>
2024-03-04 12:01:32 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const App = () => {
+   return (
+      <div>
+         App
+         <Button variant="contained">Contained</Button>
+      </div>
+   );
+};
+
+export default App;
